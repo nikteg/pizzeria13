58715281b54224bd74536c88edbe23d992b00ba6
Niklas <me@sodapop.se>
2020-01-02 00:30:01 +0100

    Updates

---
+++ precache-manifest.28c62c4b4466e198d644f0e0c437dc35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221e6e57f54949087e07041353e689cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35524f12b8d67884cbfe",
+    "url": "/static/css/main.a8b8d9b3.chunk.css"
+  },
+  {
+    "revision": "6cca72dcf2b5d45a3c26",
+    "url": "/static/js/2.6a94d647.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6a94d647.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35524f12b8d67884cbfe",
+    "url": "/static/js/main.cf0eb4ed.chunk.js"
+  },
+  {
+    "revision": "8fa79bb32885bec4cd97",
+    "url": "/static/js/runtime-main.e638555d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
